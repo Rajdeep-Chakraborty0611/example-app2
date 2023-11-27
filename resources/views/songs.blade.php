03beffc9228dacf042b1a77e0b2d2c0a07de94bd
Rajdeep <rajdeepchakraborty0611@gmail.com>
2023-11-27 22:56:32 +0530

    songs-list layout

--- resources/views/songs.blade.php
+++ resources/views/songs.blade.php
@@ -1,58 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>My Songs</title>
-    <!-- Add your CSS stylesheet link here -->
-    <link rel="stylesheet" href="styles.css">
-</head>
-<body>
-    <!-- Navigation bar -->
-    <nav>
-        <ul>
-            <li><a href="#">Home</a></li>
-            <li><a href="#">Artists</a></li>
-            <li><a href="#">Genres</a></li>
-            <li><a href="#">Playlists</a></li>
-            <li><a href="#">Contact</a></li>
-        </ul>
-    </nav>
-
-    <!-- Main content -->
-    <main>
-        <h1>My Favorite Songs</h1>
-        <div class="song-list">
-            <!-- Add your song entries here -->
-            <div class="song">
-                <h2>Title 1</h2>
-                <p>Artist: Artist 1</p>
-                <p>Genre: Pop</p>
-                <p>Tempo: 120 BPM</p>
-            </div>
-            <div class="song">
-                <h2>Title 2</h2>
-                <p>Artist: Artist 2</p>
-                <p>Genre: Rock</p>
-                <p>Tempo: 140 BPM</p>
-            </div>
-            <!-- Add more songs as needed -->
-        </div>
-        <table>
-<tr>
-<th>Title</th>
-<th>Artist</th>
-</tr>
-@foreach ($songs as $song)
-<tr>
-<td>{{ $song->title }}</td>
-<td>{{ $song->artist }}</td>
-</tr>
-@endforeach
-</table>
-    </main>
-
-    <!-- Add your JavaScript scripts here -->
-    <script src="app.js"></script>
-</body>
-</html>
+<x-layouts.main pageName='All Songs'>
+@section('content')
+<x-songs-list :songs="$songs">
+</x-songs-list>
+@endsection
+</x-layouts.main>
