caad5884f71695a85aacd62630ee8f45df2d3be8
Rajdeep <rajdeepchakraborty0611@gmail.com>
2023-11-27 23:44:40 +0530

    Create a Playlist page

--- resources/views/components/layouts/main.blade.php
+++ resources/views/components/layouts/main.blade.php
@@ -26,6 +26,12 @@
             <button>New Playlist</button>
         </div>
     </header>
+    <header class="nav">
+        <h1 class="playlistName">Playlist Name</h1>
+        <h1 class="playlistName">Playlist-1</h1>
+        <button class="b1">Edit Playlist</button>
+        <button class="b2">New Playlist</button>
+    </header>
 
     <!-- Main Content -->
     <main class="main-content">
